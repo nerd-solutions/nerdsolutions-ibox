aa83607f3f4ef3da7df40752f6aa1586445bae54
mookiejones <charles.heath.berman@gmail.com>
2015-07-07 12:31:22 -0400

    updated

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'ibox',
+  name: 'nerd.solutions:ibox',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Ibox controls',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/nerd-solutions/nerdsolutions-ibox.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -39,6 +39,6 @@ Package.onUse(function(api) {
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('ibox');
+  api.use('nerd.solutions:ibox');
   api.addFiles('ibox-tests.js');
 });
